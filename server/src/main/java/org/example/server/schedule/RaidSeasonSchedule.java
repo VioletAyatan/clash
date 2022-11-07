5bb605ca7e0263190432239c49c46ed588e6a04e
Lichengkun <lichengkun@csrd.cn>
2022-11-07 12:02:59 +0800

    :wrench: 修改编译配置

---
+++ server/src/main/java/org/example/server/schedule/RaidSeasonSchedule.java
@@ -0,0 +1,36 @@
+package org.example.server.schedule;
+
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.example.api.ClashApi;
+import org.example.api.exception.ClashApiException;
+import org.example.api.pojo.ClanCapital;
+import org.example.api.pojo.ClanResult;
+import org.example.server.dao.RaidSeasonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RaidSeasonSchedule {
+    @Autowired
+    private RaidSeasonRepository raidSeasonRepository;
+    @Autowired
+    private ClashApi clashApi;
+
+    /**
+     * 突袭周末定时任务.
+     * 设定每周二零点运行.
+     */
+    @Scheduled(cron = "0 0 0 ? * 3 *")
+    public void raidSeasonScheduling() {
+        log.info("Starting collected raid-season information in [{}]", DateUtil.format(DateUtil.date(), DatePattern.NORM_DATETIME_PATTERN));
+        try {
+            ClanResult<ClanCapital> raidSeason = clashApi.getClanCapitalRaidSeason("#280Y0YGPJ", 1);
+        } catch (ClashApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
