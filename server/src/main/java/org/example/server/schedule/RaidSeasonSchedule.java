3a2234faed84a94c33c9acc7e752004aeec467bf
Lichengkun <lichengkun@csrd.cn>
2022-11-07 16:30:27 +0800

    :construction: Work in progress.

--- server/src/main/java/org/example/server/schedule/RaidSeasonSchedule.java
+++ server/src/main/java/org/example/server/schedule/RaidSeasonSchedule.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class RaidSeasonSchedule {
+
     @Autowired
     private RaidSeasonRepository raidSeasonRepository;
     @Autowired
@@ -24,13 +25,13 @@ public class RaidSeasonSchedule {
      * 突袭周末定时任务.
      * 设定每周二零点运行.
      */
-    @Scheduled(cron = "0 0 0 ? * 3 *")
+    @Scheduled(cron = "0 0 0 ? * 3")
     public void raidSeasonScheduling() {
         log.info("Starting collected raid-season information in [{}]", DateUtil.format(DateUtil.date(), DatePattern.NORM_DATETIME_PATTERN));
         try {
             ClanResult<ClanCapital> raidSeason = clashApi.getClanCapitalRaidSeason("#280Y0YGPJ", 1);
         } catch (ClashApiException e) {
-            e.printStackTrace();
+            System.err.println("API接口调用出错，原因：" + e.getMessage() + " 详情：" + e.getDetailMessage());
         }
     }
 }
