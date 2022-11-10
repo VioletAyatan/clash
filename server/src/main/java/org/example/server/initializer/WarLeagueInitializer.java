053b50549823dfd295fc007f947fbe226ab9e367
Lichengkun <lichengkun@csrd.cn>
2022-11-10 15:23:05 +0800

    commit

--- server/src/main/java/org/example/server/initializer/WarLeagueInitializer.java
+++ server/src/main/java/org/example/server/initializer/WarLeagueInitializer.java
@@ -17,5 +17,6 @@ public class WarLeagueInitializer implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() {
+        clashApi.getClanWarLeagueInformation("");
     }
 }
