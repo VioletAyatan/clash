a26715cb25069dae18440239320a7837eac08164
lichengkun <lichengkun@csrd.cn>
2023-02-06 17:10:27 +0800

    :construction: 配置属性添加，更改clash-api获取令牌的逻辑

--- server/src/main/java/org/example/server/config/ClashApiConfig.java
+++ server/src/main/java/org/example/server/config/ClashApiConfig.java
@@ -1,16 +1,29 @@
 package org.example.server.config;
 
+import cn.hutool.core.util.StrUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.api.ClashApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class ClashApiConfig {
+
+    private final ClashProperties clashProperties;
+    private static final String ENV_TOKEN_NAME = "CLASH_TOKEN";
 
     @Bean
     public ClashApi clashApi() {
-        String clashToken = System.getenv("CLASH_TOKEN"); //读取环境变量里的token
-        System.out.println("Token = " + clashToken);
+        String clashToken;
+        if (StrUtil.isNotBlank(clashProperties.getApiAccessToken())) {
+            clashToken = clashProperties.getApiAccessToken();
+        } else {
+            clashToken = System.getenv(ENV_TOKEN_NAME);
+        }
+        log.debug("[DEBUG] Clash-API is initialized with access-token [{}]", clashToken);
         return new ClashApi(clashToken);
     }
 }
--- server/src/main/java/org/example/server/config/ClashProperties.java
+++ server/src/main/java/org/example/server/config/ClashProperties.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = ClashProperties.PREFIX)
 public class ClashProperties {
-    public static final String PREFIX = "clash";
+    public static final String PREFIX = "clash-manager";
     /**
      * 设置需要管理的氏族.
      */
     private String clanTag;
+    /**
+     * 访问官方Api接口所需的Token，如何获取请参考：<a href="https://developer.clashofclans.com/#/getting-started">developer.clashofclans.com</a>
+     */
+    private String apiAccessToken;
 }
