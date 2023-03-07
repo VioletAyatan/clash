7bbc3be3c1542c42ef77a6b7ea1ffe4cdfe4c8e8
Ankol <ankol2329340232@gmail.com>
2023-03-07 19:21:08 +0800

    commit

--- server/src/main/java/org/example/server/config/ClashProperties.kt
+++ server/src/main/java/org/example/server/config/ClashProperties.kt
@@ -1,20 +1,24 @@
-package org.example.server.config;
+package org.example.server.config
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import lombok.Data
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 
 @Data
 @Component
 @ConfigurationProperties(prefix = ClashProperties.PREFIX)
-public class ClashProperties {
-    public static final String PREFIX = "clash-manager";
+class ClashProperties {
     /**
      * 设置需要管理的氏族.
      */
-    private String clanTag;
+    private val clanTag: String? = null
+
     /**
-     * 访问官方Api接口所需的Token，如何获取请参考：<a href="https://developer.clashofclans.com/#/getting-started">developer.clashofclans.com</a>
+     * 访问官方Api接口所需的Token，如何获取请参考：[developer.clashofclans.com](https://developer.clashofclans.com/#/getting-started)
      */
-    private String apiAccessToken;
+    private val apiAccessToken: String? = null
+
+    companion object {
+        const val PREFIX = "clash-manager"
+    }
 }
