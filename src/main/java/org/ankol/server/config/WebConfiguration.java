2297103febde47e5395cdb8858fe316bfdf517ae
lichengkun <lichengkun@csrd.cn>
2023-11-06 09:15:20 +0800

    添加Tomcatservlet定制器

--- src/main/java/org/ankol/server/config/WebConfiguration.java
+++ src/main/java/org/ankol/server/config/WebConfiguration.java
@@ -1,11 +1,15 @@
 package org.ankol.server.config;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration implements WebMvcConfigurer, WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -15,5 +19,10 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
     }
 }
